d9cd11b8539cb04247210d760d94a746817bc638
samuelejakupi <67373923+samuelejakupi@users.noreply.github.com>
2026-08-05 16:46:08 +0200

    Corregge percorso immagini emblemi

--- app/page.tsx
+++ app/page.tsx
@@ -1079,7 +1079,7 @@ function BadgeList({ badges }: { badges: Badge[] }) {
         >
           <div className="badge-emblem" aria-hidden="true">
             <span className="badge-crown">◆</span>
-            <span className="badge-icon"><Image src={`/emblems/${badge.glyph}.png`} alt="" width={58} height={58} /></span>
+            <span className="badge-icon"><Image src={`${basePath}/emblems/${badge.glyph}.png`} alt="" width={58} height={58} /></span>
             <span className="badge-laurel badge-laurel-left">❯</span>
             <span className="badge-laurel badge-laurel-right">❮</span>
           </div>
